1ff146fa9d073fd267f0ec07310ebcc37c93f91a
MOU Rui <rui.mou@ck-telecom.com>
2012-03-10 20:14:38 +0800

    2012/3/10 Update new send message

--- Android/src/com/mrpcsync/android/data/message/MrPcSyncMessage.java
+++ Android/src/com/mrpcsync/android/data/message/MrPcSyncMessage.java
@@ -1,6 +1,5 @@
 package com.mrpcsync.android.data.message;
 
-
 import java.util.Iterator;
 import java.util.List;
 
@@ -8,26 +7,63 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.telephony.SmsManager;
+
 public class MrPcSyncMessage {
-    private static MrPcSyncMessage _instance = null;
-    private SmsManager mSms;
+	private static MrPcSyncMessage _instance = null;
+	private SmsManager mSms;
 
-    private MrPcSyncMessage() {
-    	mSms = SmsManager.getDefault();
-    }
+	private MrPcSyncMessage() {
+		mSms = SmsManager.getDefault();
+	}
 
-    public static MrPcSyncMessage Instance() {
-        if (_instance == null) {
-            _instance = new MrPcSyncMessage();
-        }
-        return _instance;
-    }
-    
-    public void sendMessage(String phone, String message){
-    	List<String> msg = mSms.divideMessage(message);
-    	Iterator<String> itor = msg.iterator();
-    	while (itor.hasNext()){
-    		mSms.sendTextMessage(phone, null, itor.next(), null, null);
-    	}
-    }
+	public static MrPcSyncMessage Instance() {
+		if (_instance == null) {
+			_instance = new MrPcSyncMessage();
+		}
+		return _instance;
+	}
+
+	public void sendMessage(Message message) {
+		List<String> msg = mSms.divideMessage(message.getMessage());
+		Iterator<String> itor = msg.iterator();
+		while (itor.hasNext()) {
+			mSms.sendTextMessage(message.getPhone(), null, itor.next(), null,
+					null);
+		}
+	}
+
+	public static class Message {
+		private String id;
+		private String phone;
+		private String message;
+
+		public Message(String phone, String message) {
+			setPhone(phone);
+			setMessage(message);
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getPhone() {
+			return phone;
+		}
+
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		public void setMessage(String message) {
+			this.message = message;
+		}
+	}
 }
